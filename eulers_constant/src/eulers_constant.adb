8f3529677becec8646875a0a4b7aab6801d32daa
Stephen Merrony <SMerrony@users.noreply.github.com>
2024-06-16 21:35:24 +0200

    Add Euler constnat approximations

--- eulers_constant/src/eulers_constant.adb
+++ eulers_constant/src/eulers_constant.adb
@@ -1,39 +1,47 @@
-with Ada.Numerics.Generic_Elementary_Functions;
-with Ada.Text_IO;
+with Ada.Numerics.Elementary_Functions;      use Ada.Numerics.Elementary_Functions;
+with Ada.Numerics.Long_Elementary_Functions; use Ada.Numerics.Long_Elementary_Functions;
+with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Eulers_Constant is
-   package LF_Funcs is new Ada.Numerics.Generic_Elementary_Functions (Long_Float);
 
-   function Euler_Simple (Iterations : Integer) return Long_Float is
-      Gamma : Long_Float := 0.0;
+   function Euler_Vacca (Iterations : Integer) return Long_Float is
+      Gamma : Long_Float   := 1.0;
+      Term  : Long_Float;
+      Power : Long_Integer;
+      Sign  : Long_Float;
    begin
-      for N in 1 .. Iterations loop
-         Gamma := Gamma + Long_Float (1.0) / Long_Float (N);
-      end loop;
-      Gamma := Gamma - LF_Funcs.Log (Long_Float (Iterations)) + 1.0 / (2.0 * Long_Float (Iterations));
-      return Gamma / 10.0;
-   end Euler_Simple;
-
-   function Euler_Macys (Iterations : Integer) return Long_Float is
-      Gamma : Long_Float := 1.0;
-      N     : Long_Float := 1.0;
-   begin
-      while N <= Long_Float (Iterations) loop
-         Gamma := Gamma + (1.0 / N) - (1.0 / (N + 1.0)) - (1.0 / ((N * N) + (N - 1.0)));
-         N := N + 1.0;
+      Gamma := 0.5 - (1.0 / 3.0);
+      for I in 2 .. Iterations loop
+         Power := 2 ** Natural (I);
+         Sign  := -1.0;
+         Term  := 0.0;
+         for Domin in Power .. (2 * Power - 1) loop
+            Sign := - (Sign);
+            Term := Term + Sign / Long_Float (Domin);
+         end loop;
+         Gamma := Gamma + (Long_Float (I) * Term);
       end loop;
       return Gamma;
-   end Euler_Macys;
+   end Euler_Vacca;
+
+   --  Ada Float type is IEEE 754 32-bit, giving 9 decimal digits of precision
+   Euler_Castellanos_Float : constant Float :=
+      (((80.0 ** 3) + 92.0) / 
+        (61.0 ** 4))           ** (1.0 / 6.0);
+
+   --  Ada Long_Float type is IEEE 754 32-bit, giving 14 decimal digits of precision
+   Euler_Castellanos_Long_Float : constant Long_Float :=
+      (990.0 ** 3 - 55.0 ** 3 - 79.0 ** 2 - 16.0) / 
+                  70.0 ** 5; 
 
    Iters : Integer;
 begin
-   Ada.Text_IO.Put_Line ("Iterations    Simple     Macys");
-   Iters := 1;
-   Ada.Text_IO.Put_Line (Iters'Image & " " & Euler_Simple (Iters)'Image & " " & Euler_Macys (Iters)'Image);
-   Iters := 10;
-   Ada.Text_IO.Put_Line (Iters'Image & " " & Euler_Simple (Iters)'Image & " " & Euler_Macys (Iters)'Image);
-   Iters := 100;
-   Ada.Text_IO.Put_Line (Iters'Image & " " & Euler_Simple (Iters)'Image & " " & Euler_Macys (Iters)'Image);
-   Iters := 1000;
-   Ada.Text_IO.Put_Line (Iters'Image & " " & Euler_Simple (Iters)'Image & " " & Euler_Macys (Iters)'Image);
+   Put_Line ("Its. Vacca");
+   Iters := 2;
+   while Iters <= 32 loop
+      Put_Line (Iters'Image & " " & Euler_Vacca (Iters)'Image);
+      Iters := Iters + 2;
+   end loop;
+   Put_Line ("Castellanos approximation for standard Float (9 digits): " & Euler_Castellanos_Float'Image);
+   Put_Line ("Castellanos approximation for Long Float (14 digits): " & Euler_Castellanos_Long_Float'Image);
 end Eulers_Constant;
