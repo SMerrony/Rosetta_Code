a31b59d71beb2d8e34ab97c6ddb5b80196783798
Stephen Merrony <SMerrony@users.noreply.github.com>
2024-06-22 09:33:32 +0200

    Tweaks and output converted to PNG

--- abelian_sandpile/src/abelian_sandpile.adb
+++ abelian_sandpile/src/abelian_sandpile.adb
@@ -1,36 +1,36 @@
 pragma Ada_2022;
-with Ada.Command_Line;
+with Ada.Command_Line;  use Ada.Command_Line;
 with Ada.Text_IO;       use Ada.Text_IO;
 procedure Abelian_Sandpile is
    type Grid_2D is array (Positive range <>, Positive range <>) of Natural;
 
    procedure Write_PPM (Grid : Grid_2D; Filename : String) is
-      PBM_File : File_Type;
+      PPM_File : File_Type;
    begin
-      Create (PBM_File, Out_File, Filename);
-      Put_Line (PBM_File, "P3");
-      Put_Line (PBM_File, Grid'Length (1)'Image & Grid'Length (2)'Image);
-      Put_Line (PBM_File, "255");
+      Create (PPM_File, Out_File, Filename);
+      Put_Line (PPM_File, "P3");
+      Put_Line (PPM_File, Grid'Length (1)'Image & Grid'Length (2)'Image);
+      Put_Line (PPM_File, "7");
       for Y in 1 .. Grid'Length (2) loop
          for X in 1 .. Grid'Length (1) loop
             case Grid (X, Y) is
-               when 0 => Put_Line (PBM_File, "0 0 0");      --  black
-               when 1 => Put_Line (PBM_File, "0 255 0");    --  green
-               when 2 => Put_Line (PBM_File, "255 0 255");  --  lilac
-               when 3 => Put_Line (PBM_File, "255 255 0");  --  yellow
-               when others => Put_Line (PBM_File, "0 0 255"); --  blue - shouldn't happen
+               when 0 => Put_Line (PPM_File, "0 0 0");      --  black
+               when 1 => Put_Line (PPM_File, "0 7 0");      --  green
+               when 2 => Put_Line (PPM_File, "7 0 7");      --  lilac
+               when 3 => Put_Line (PPM_File, "7 7 0");      --  yellow
+               when others => Put_Line (PPM_File, "0 0 7"); --  blue, shouldn't happen
             end case;
          end loop;
       end loop;
-      Close (PBM_File);
+      Close (PPM_File);
    end Write_PPM;
 begin
-   if Ada.Command_Line.Argument_Count /= 2 then
+   if Argument_Count /= 2 then
       Put_Line ("Error: Must specify <Sandpile Height> <Grid Size>");
    else
       declare
-         Initial_Height : constant Positive := Positive'Value (Ada.Command_Line.Argument (1));
-         Grid_Size      : constant Positive := Positive'Value (Ada.Command_Line.Argument (2));
+         Initial_Height : constant Positive := Positive'Value (Argument (1));
+         Grid_Size      : constant Positive := Positive'Value (Argument (2));
          Sandpile       : Grid_2D (1 .. Grid_Size, 1 .. Grid_Size) := [others => [others => 0]];
          More_To_Do     : Boolean := True;
          Overspill      : Natural;
@@ -41,7 +41,6 @@ begin
             for X in 1 .. Grid_Size loop
                for Y in 1 .. Grid_Size loop
                   if Sandpile (X, Y) >= 4 then
-                     --  Sandpile (X, Y) := @ - 4;
                      Overspill := Sandpile (X, Y) / 4;
                      Sandpile (X, Y) :=  @ mod 4;
                      More_To_Do := True;
@@ -61,7 +60,7 @@ begin
                end loop;
             end loop;
          end loop;
-         if Grid_Size < 16  then Put_Line (Sandpile'Image); end if;
+         if Grid_Size < 16 then Put_Line (Sandpile'Image); end if;
          Write_PPM (Sandpile, "sandpile.ppm");
       end;
    end if;
